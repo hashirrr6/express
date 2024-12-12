de011aebd51788387c11744d2fecf871986a24ae
hashirrr6 <hashi3904@gmail.com>
2024-12-12 23:58:42 +0530

    s

--- middleware/server.js
+++ middleware/server.js
@@ -14,35 +14,91 @@
 // })
 
 
-const express=require('express')
-const app=express();
-app.use(express.json())
 
-app.post("/",token,(req,res)=>{
-    console.log("endpoint");
 
-    console.log(req.body);
+// sample>
 
-    res.send("home")
-    
-    
-})
-app.listen(3000,()=>{
-    console.log("server started");
-    
-})
 
-function token(req,res,next){
-    console.log("middle");
-    console.log(req.body);
-    next()
-    
-    
-}
 
-// const express=require('express');
+
+// const express=require('express')
+// var bodyParser =require('body-parser')
+// const { log } = require('console')
+// const app=express()
+// app.use(bodyParser.urlencoded())
+// app.post("/",(req,res)=>{
+//     console.log(req.body);
+//     res.send("Homieeeee")
+    
+// })
+// app.listen(3000,()=>{
+//     console.log("lessgoooo");
+    
+// })
+
+
+//////////////////////////////////
+
+
+// const express=require('express')
 // const app=express();
 // app.use(express.json())
+
+// app.post("/",token,(req,res)=>{
+//     console.log("endpoint");
+
+//     console.log(req.body);
+//     res.send("home")  
+// })
+// app.listen(3000,()=>{
+//     console.log("server started");
+    
+// })
+// function token(req,res,next){
+//     console.log("middle");
+//     console.log(req.body);
+//     next() 
+// }
+
+
+
+
+// SamPle work >
+
+// const express=require("express")
+// const app=express();
+// app.use(express.json())
+
+// app.post("/",token,(req,res)=>{
+//     console.log("end-POint");
+//     console.log(req.body);
+//     res.send("go hOmie")
+// })
+// app.listen(3000,()=>{
+//     console.log("less gOOOO");
+    
+// })
+// function token(req,res,next){
+//     console.log("middle");
+//     console.log(req.body);
+//     next()
+    
+    
+// }
+
+
+
+
+/////////////////////////////////////////////////
+
+
+
+
+
+
+const express=require('express');
+const app=express();
+app.use(express.json())
 
 // app.post("/",token,(req,res)=>{
 //     console.log("endpoint");
@@ -52,22 +108,23 @@ function token(req,res,next){
     
 // })
 
-// app.get("/data",(req,res)=>{
-//     // const{id}=req.params
-//     console.log(req.query);
-//     res.send("data")
+app.get("/data",(req,res)=>{
+    // const{id}=req.params
+    console.log(req.query);
+    // console.log(req.params);
+    res.send("data")
     
-// })
-// app.listen(3000,()=>{
-//     console.log("server created");
+})
+app.listen(3001,()=>{
+    console.log("server created");
     
 
-// })
+})
 
-// function token(req,res,next){
-//     console.log("middle");
+function token(req,res,next){
+    console.log("middle");
 
-//     console.log(req.body);
-//     next()
+    console.log(req.body);
+    next()
     
-// }
+}
